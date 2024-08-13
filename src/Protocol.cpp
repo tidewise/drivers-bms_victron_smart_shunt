ccb4b231d880d17c251a385e1d9bd5a05557a479
seixasxbr <mateus.seixas@tidewise.io>
2024-08-12 23:37:22 -0300

    feat: add protocol dummy

---
+++ src/Protocol.cpp
@@ -0,0 +1,4 @@
+#include <bms_victron_smart_shunt/Protocol.hpp>
+
+using namespace bms_victron_smart_shunt;
+
---
+++ src/Protocol.hpp
@@ -0,0 +1,9 @@
+#ifndef BMS_VICTRON_SMART_SHUNT_PROTOCOL_HPP
+#define BMS_VICTRON_SMART_SHUNT_PROTOCOL_HPP
+
+namespace bms_victron_smart_shunt {
+    namespace protocol {
+    }
+}
+
+#endif
