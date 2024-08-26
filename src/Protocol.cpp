0a5925219518814a85c2edb647c1e34e4d6a898e
seixasxbr <mateus.seixas@tidewise.io>
2024-08-26 10:24:19 -0300

    fix: fix substring and use stoi instead stringstream

--- src/Protocol.cpp
+++ src/Protocol.cpp
@@ -19,8 +19,6 @@ int protocol::extractPacket(const uint8_t* buffer, int buffer_size)
     if (packet_start) {
         return -packet_start;
     }
-    std::vector<uint8_t> checksum_vector(CHECKSUM_STR,
-        CHECKSUM_STR + std::strlen(CHECKSUM_STR));
     auto checksum_begin_it = std::search(buffer,
         buffer + buffer_size,
         CHECKSUM_STR,
@@ -29,7 +27,7 @@ int protocol::extractPacket(const uint8_t* buffer, int buffer_size)
         // There is still no checksum, wait for more bytes
         return 0;
     }
-    int packet_size = checksum_begin_it - buffer + checksum_vector.size() + 1;
+    int packet_size = checksum_begin_it - buffer + CHECKSUM_STR_LEN + 2;
     int checksum = 0;
     for (int i = 0; i < packet_size; i++) {
         // Take modulo 256 in account
@@ -51,120 +49,146 @@ SmartShuntFeedback protocol::parseSmartShuntFeedback(uint8_t const* buffer,
     std::string data_str(reinterpret_cast<const char*>(buffer), buffer_size);
     std::istringstream stream(data_str);
     std::string line;
-    while (std::getline(stream, line, char(LINE_DELIMITER))) {
-        size_t field_delimiter_pos = line.find(FIELD_DELIMITER);
-        std::string field;
-        std::string value;
-        if (field_delimiter_pos != std::string::npos) {
-            field = line.substr(0, field_delimiter_pos);
-            value = line.substr(field_delimiter_pos + 1);
-            value.erase(value.size() - 1);
-        }
-        int val;
-        std::stringstream(value) >> val;
-        if (field == "V") {
-            data.voltage = static_cast<float>(val) / 1000;
-        }
-        else if (field == "VM") {
-            data.mid_point_voltage = static_cast<float>(val) / 1000;
-        }
-        else if (field == "VS") {
-            data.auxiliary_voltage = static_cast<float>(val) / 1000;
-        }
-        else if (field == "DM") {
-            data.mid_point_deviation = val;
-        }
-        else if (field == "I") {
-            data.current = static_cast<float>(val) / 1000;
-        }
-        else if (field == "T") {
-            data.temperature = base::Temperature::fromCelsius(val);
-        }
-        else if (field == "P") {
-            data.instantaneous_power = val;
-        }
-        else if (field == "CE") {
-            data.consumed_charge = static_cast<float>(val) * 3.6;
-        }
-        else if (field == "SOC") {
-            data.state_of_charge = val;
-        }
-        else if (field == "TTG") {
-            data.time_to_go = base::Time::fromSeconds(val * 60);
-        }
-        else if (field == "Alarm") {
-            data.alarm_condition_active = value;
-        }
-        else if (field == "Relay") {
-            data.relay_state = value;
-        }
-        else if (field == "AR") {
-            data.alarm_reason = Alarms(val);
-        }
-        else if (field == "H1") {
-            data.deepest_discharge_depth = static_cast<float>(val) * 3.6;
-        }
-        else if (field == "H2") {
-            data.last_discharge_depth = static_cast<float>(val) * 3.6;
-        }
-        else if (field == "H3") {
-            data.average_discharge_depth = static_cast<float>(val) * 3.6;
-        }
-        else if (field == "H4") {
-            data.charge_cycles_number = val;
-        }
-        else if (field == "H5") {
-            data.full_discharges_number = val;
-        }
-        else if (field == "H6") {
-            data.cumulative_charge_drawn = static_cast<float>(val) * 3.6;
-        }
-        else if (field == "H7") {
-            data.minimum_voltage = static_cast<float>(val) / 1000;
-        }
-        else if (field == "H8") {
-            data.maximum_voltage = static_cast<float>(val) / 1000;
-        }
-        else if (field == "H9") {
-            data.seconds_since_last_full_charge = base::Time::fromSeconds(val);
-        }
-        else if (field == "H10") {
-            data.automatic_synchronizations_number = val;
-        }
-        else if (field == "H11") {
-            data.low_voltage_alarms_number = val;
-        }
-        else if (field == "H12") {
-            data.high_voltage_alarms_number = val;
-        }
-        else if (field == "H13") {
-            data.minimum_auxiliary_voltage = static_cast<float>(val) / 1000;
-        }
-        else if (field == "H14") {
-            data.maximum_auxiliary_voltage = static_cast<float>(val) / 1000;
-        }
-        else if (field == "H17") {
-            data.discharged_energy = val * 36000;
-        }
-        else if (field == "H18") {
-            data.charged_energy = val * 36000;
-        }
-        else if (field == "BMV") {
-            data.model_description = value;
-        }
-        else if (field == "FWE") {
-            data.firmware_version = value;
-        }
-        else if (field == "PID") {
-            data.product_id = value;
-        }
-        else if (field == "MON") {
-            if (val < SolarCharger || val > WaterHeater) {
-                ostringstream msg;
-                msg << "unsupported DC Monitor Mode " << value;
-                throw invalid_argument(msg.str());
+    while (std::getline(stream, line, PACKET_START_MARKER)) {
+        if (!line.empty()) {
+            size_t field_delimiter_pos = line.find(FIELD_DELIMITER);
+            if (field_delimiter_pos == std::string::npos) {
+                throw std::runtime_error("invalid line found " + line);
             }
-            data.dc_monitor_mode = DCMonitorMode(val);
+            string field = line.substr(1, field_delimiter_pos - 1);
+            string value_s = line.substr(field_delimiter_pos + 1, line.size() - 1);
+            if (field == "V") {
+                int val = stoi(value_s);
+                data.voltage = static_cast<float>(val) / 1000;
+            }
+            else if (field == "VM") {
+                int val = stoi(value_s);
+                data.mid_point_voltage = static_cast<float>(val) / 1000;
+            }
+            else if (field == "VS") {
+                int val = stoi(value_s);
+                data.auxiliary_voltage = static_cast<float>(val) / 1000;
+            }
+            else if (field == "DM") {
+                int val = stoi(value_s);
+                data.mid_point_deviation = val;
+            }
+            else if (field == "I") {
+                int val = stoi(value_s);
+                data.current = static_cast<float>(val) / 1000;
+            }
+            else if (field == "T") {
+                int val = stoi(value_s);
+                data.temperature = base::Temperature::fromCelsius(val);
+            }
+            else if (field == "P") {
+                int val = stoi(value_s);
+                data.instantaneous_power = val;
+            }
+            else if (field == "CE") {
+                int val = stoi(value_s);
+                data.consumed_charge = static_cast<float>(val) * 3.6;
+            }
+            else if (field == "SOC") {
+                int val = stoi(value_s);
+                data.state_of_charge = val;
+            }
+            else if (field == "TTG") {
+                int val = stoi(value_s);
+                data.time_to_go = base::Time::fromSeconds(val * 60);
+            }
+            else if (field == "Alarm") {
+                data.alarm_condition_active = value_s;
+            }
+            else if (field == "Relay") {
+                data.relay_state = value_s;
+            }
+            else if (field == "AR") {
+                int val = stoi(value_s);
+                data.alarm_reason = Alarms(val);
+            }
+            else if (field == "H1") {
+                int val = stoi(value_s);
+                data.deepest_discharge_depth = static_cast<float>(val) * 3.6;
+            }
+            else if (field == "H2") {
+                int val = stoi(value_s);
+                data.last_discharge_depth = static_cast<float>(val) * 3.6;
+            }
+            else if (field == "H3") {
+                int val = stoi(value_s);
+                data.average_discharge_depth = static_cast<float>(val) * 3.6;
+            }
+            else if (field == "H4") {
+                int val = stoi(value_s);
+                data.charge_cycles_number = val;
+            }
+            else if (field == "H5") {
+                int val = stoi(value_s);
+                data.full_discharges_number = val;
+            }
+            else if (field == "H6") {
+                int val = stoi(value_s);
+                data.cumulative_charge_drawn = static_cast<float>(val) * 3.6;
+            }
+            else if (field == "H7") {
+                int val = stoi(value_s);
+                data.minimum_voltage = static_cast<float>(val) / 1000;
+            }
+            else if (field == "H8") {
+                int val = stoi(value_s);
+                data.maximum_voltage = static_cast<float>(val) / 1000;
+            }
+            else if (field == "H9") {
+                int val = stoi(value_s);
+                data.seconds_since_last_full_charge = base::Time::fromSeconds(val);
+            }
+            else if (field == "H10") {
+                int val = stoi(value_s);
+                data.automatic_synchronizations_number = val;
+            }
+            else if (field == "H11") {
+                int val = stoi(value_s);
+                data.low_voltage_alarms_number = val;
+            }
+            else if (field == "H12") {
+                int val = stoi(value_s);
+                data.high_voltage_alarms_number = val;
+            }
+            else if (field == "H13") {
+                int val = stoi(value_s);
+                data.minimum_auxiliary_voltage = static_cast<float>(val) / 1000;
+            }
+            else if (field == "H14") {
+                int val = stoi(value_s);
+                data.maximum_auxiliary_voltage = static_cast<float>(val) / 1000;
+            }
+            else if (field == "H17") {
+                int val = stoi(value_s);
+                data.discharged_energy = val * 36000;
+            }
+            else if (field == "H18") {
+                int val = stoi(value_s);
+                data.charged_energy = val * 36000;
+            }
+            else if (field == "BMV") {
+                data.model_description = value_s;
+            }
+            else if (field == "FWE") {
+                data.firmware_version = value_s;
+            }
+            else if (field == "PID") {
+                data.product_id = value_s;
+            }
+            else if (field == "MON") {
+                int val = stoi(value_s);
+                if (val < SOLAR_CHARGER || val > WATER_HEATER) {
+                    ostringstream msg;
+                    msg << "unsupported DC Monitor Mode " << value_s;
+                    throw invalid_argument(msg.str());
+                }
+                data.dc_monitor_mode = DCMonitorMode(val);
+            }
         }
     }
     return data;
